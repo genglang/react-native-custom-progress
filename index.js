e249021baff0dbce60ebb96e9260cec67a7ea620
genglang <657578302@qq.com>
2018-12-27 00:33:54 +0800

    导出模块

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native'
+import App from './App'
+import {name as appName} from './app.json'
+import AnimatedCircleProgress from './src/CircleProgress'
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App)
+
+module.exports = {
+    CircleProgress: AnimatedCircleProgress
+}
